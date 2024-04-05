a1bc7aa88b6367fef48c468622a0306e5ba4fd51
s-biagioni <116901303+s-biagioni@users.noreply.github.com>
2024-04-05 22:13:15 +0100

    Update string_embedding_and_visualisation.py

--- 0001_embeddings/streamlit/string_embedding_and_visualisation.py
+++ 0001_embeddings/streamlit/string_embedding_and_visualisation.py
@@ -9,103 +9,109 @@ import seaborn
 from scipy import spatial
 import numpy as np
 from sklearn.decomposition import PCA
-import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+import streamlit as st
 
 
+st.title("Create Embeddings and Visualise 2D PCA")
 
-
+st.info("""Please enter your OpenAI key in the panel on the left 
+            and write the 3 strings to create and visualise their embeddings.""")
 
 # The OpenAI key is stored in a file.
-key_location = '/Users/silvi/Downloads/key-location/genaikey.txt'
-
-with open(key_location, 'r') as file:
-    key = file.readline().strip()
+openai.api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
 
-openai.api_key = key
-
-
-string1 = "A koala jumped from a tree"
-string2 = "A koala is on a tree"
-string3 = "I like pizza"
-
-
-# https://platform.openai.com/docs/guides/embeddings/what-are-embeddings?lang=python
-
-response = openai.embeddings.create(
-    input=[string1, string2, string3],
-    model="text-embedding-3-small"
+string1 = st.text_input(
+    "Enter some text 👇",
+    placeholder="string 1",
 )
 
-# print(response)
-# print(response.data[0].embedding)
-# print(response.data[1].embedding)
-# print(response.data[2].embedding)
+string2 = st.text_input(
+    "Enter some text 👇",
+    placeholder="string 2",
+)
+
+string3 = st.text_input(
+    "Enter some text 👇",
+    placeholder="string 3",
+)
+
+if string1 and string2 and string3 and openai.api_key.startswith('sk-'):
+    # https://platform.openai.com/docs/guides/embeddings/what-are-embeddings?lang=python
+
+    response = openai.embeddings.create(
+        input=[string1, string2, string3],
+        model="text-embedding-3-small"
+    )
+
+    # print(response)
+    # print(response.data[0].embedding)
+    # print(response.data[1].embedding)
+    # print(response.data[2].embedding)
 
 
-# https://stackoverflow.com/a/18424933
+    # https://stackoverflow.com/a/18424933
 
-cosine_similarity1 = 1 - spatial.distance.cosine(response.data[0].embedding, response.data[1].embedding)
+    cosine_similarity1 = 1 - spatial.distance.cosine(response.data[0].embedding, response.data[1].embedding)
 
-print('cosine similarity(string1, string2): ')
-print(cosine_similarity1)
+    st.write('cosine similarity(string1, string2): ')
+    st.write(cosine_similarity1)
 
-cosine_similarity2 = 1 - spatial.distance.cosine(response.data[1].embedding, response.data[2].embedding)
+    cosine_similarity2 = 1 - spatial.distance.cosine(response.data[1].embedding, response.data[2].embedding)
 
-print('cosine similarity(string2, string3): ')
-print(cosine_similarity2)
+    st.write('cosine similarity(string2, string3): ')
+    st.write(cosine_similarity2)
 
-cosine_similarity3 = 1 - spatial.distance.cosine(response.data[0].embedding, response.data[2].embedding)
+    cosine_similarity3 = 1 - spatial.distance.cosine(response.data[0].embedding, response.data[2].embedding)
 
-print('cosine similarity(string1, string3): ')
-print(cosine_similarity3)
+    st.write('cosine similarity(string1, string3): ')
+    st.write(cosine_similarity3)
 
+    # heatmap among equal strings
+    seaborn.heatmap([response.data[0].embedding[:20], response.data[1].embedding[:20],response.data[2].embedding[:20]], 
+                    cbar=False, xticklabels=False, yticklabels=[string1, string2, string3]
+                    )
 
+    # https://discuss.streamlit.io/t/how-to-display-matplotlib-graphs-in-streamlit-application/35383/2
+    st.pyplot(plt.gcf()) # instead of plt.show()
 
-# heatmap among equal strings
-seaborn.heatmap([response.data[0].embedding[:20], response.data[1].embedding[:20],response.data[2].embedding[:20]], 
-                cbar=False, xticklabels=False, yticklabels=[string1, string2, string3]
-                )
+    # store the embeddings in a 2d numpy array
+    embeddings = [response.data[0].embedding, 
+                response.data[1].embedding, 
+                response.data[2].embedding]
+    embeddings_array = np.array(embeddings)
 
-plt.show()
-
-# store the embeddings in a 2d numpy array
-embeddings = [response.data[0].embedding, 
-              response.data[1].embedding, 
-              response.data[2].embedding]
-embeddings_array = np.array(embeddings)
-
-print("Shape: " + str(embeddings_array.shape))
-print(embeddings_array)
-
+    st.write("Shape: " + str(embeddings_array.shape))
+    st.write(embeddings_array)
 
 
-# reducing the embedding dimension with PCA
-# https://learn.deeplearning.ai/courses/google-cloud-vertex-ai/lesson/4/visualizing-embeddings
+    # reducing the embedding dimension with PCA
+    # https://learn.deeplearning.ai/courses/google-cloud-vertex-ai/lesson/4/visualizing-embeddings
 
-# Perform PCA for 2D visualization
-PCA_model = PCA(n_components = 2)
-PCA_model.fit(embeddings_array)
-new_values = PCA_model.transform(embeddings_array)
-
-
-print("Shape: " + str(new_values.shape))
-print(new_values)
+    # Perform PCA for 2D visualization
+    PCA_model = PCA(n_components = 2)
+    PCA_model.fit(embeddings_array)
+    new_values = PCA_model.transform(embeddings_array)
 
 
-print(new_values[:,0])
-print(new_values[:,1])
+    st.write("PCA Shape: " + str(new_values.shape))
+    st.write(new_values)
 
 
-colours = ListedColormap(['r','b','g'])
-values = [0, 1, 2]
-scatter = plt.scatter(new_values[:,0], new_values[:,1], c=values, cmap=colours)
-classes = [string1, string2, string3]
-plt.legend(handles=scatter.legend_elements()[0], labels=classes)
+    st.write(new_values[:,0])
+    st.write(new_values[:,1])
 
-plt.axis('square')
 
-plt.show()
+    colours = ListedColormap(['r','b','g'])
+    values = [0, 1, 2]
+    scatter = plt.scatter(new_values[:,0], new_values[:,1], c=values, cmap=colours)
+    classes = [string1, string2, string3]
+    plt.legend(handles=scatter.legend_elements()[0], labels=classes)
+
+    plt.axis('square')
+
+    # https://discuss.streamlit.io/t/how-to-display-matplotlib-graphs-in-streamlit-application/35383/2
+    st.pyplot(plt.gcf()) # instead of plt.show()
 
